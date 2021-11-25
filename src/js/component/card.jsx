162e34a6d565f254016537ac80d27b68910d1911
Saray Budin Aravena <sarybudin@gmail.com>
2021-11-24 22:43:34 -0300

    first commit

---
+++ src/js/component/card.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+
+export const Card = () => {
+	return (
+		<div className="d-flex justify-content-between row">
+			<div className="col-12 col-sm-6 col-md-3">
+				<div className="card">
+					<img
+						src="http://lorempixel.com/300/200"
+						className="card-img-top"
+						alt="..."
+					/>
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">
+							Some quick example text to build on the card title
+							and make up the bulk of the cards content.
+						</p>
+						<a href="#" className="btn btn-primary">
+							Go somewhere
+						</a>
+					</div>
+				</div>
+			</div>
+			<div className="col-12 col-sm-6 col-md-3">
+				<div className="card">
+					<img
+						src="http://lorempixel.com/300/200"
+						className="card-img-top"
+						alt="..."
+					/>
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">
+							Some quick example text to build on the card title
+							and make up the bulk of the cards content.
+						</p>
+						<a href="#" className="btn btn-primary">
+							Go somewhere
+						</a>
+					</div>
+				</div>
+			</div>
+			<div className="col-12 col-sm-6 col-md-3">
+				<div className="card">
+					<img
+						src="http://lorempixel.com/300/200"
+						className="card-img-top"
+						alt="..."
+					/>
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">
+							Some quick example text to build on the card title
+							and make up the bulk of the cards content.
+						</p>
+						<a href="#" className="btn btn-primary">
+							Go somewhere
+						</a>
+					</div>
+				</div>
+			</div>
+			<div className="col-12 col-sm-6 col-md-3">
+				<div className="card">
+					<img
+						src="http://lorempixel.com/300/200"
+						className="card-img-top"
+						alt="..."
+					/>
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">
+							Some quick example text to build on the card title
+							and make up the bulk of the cards content.
+						</p>
+						<a href="#" className="btn btn-primary">
+							Go somewhere
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,17 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { Card } from "./card.jsx";
+import { Jumbotron } from "./jumbotron.jsx";
+import { Navbar } from "./navbar.jsx";
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<Navbar />
+			<div className="ps-3 pe-3 pt-3 pb-3">
+				<Jumbotron />
+				<Card />
+			</div>
 		</div>
 	);
 };
